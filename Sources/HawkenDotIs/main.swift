c9b3b226991ff2c953a07a8c5230ef65a0bc1bd0
Hawken Rives <rives@stolaf.edu>
2020-06-20 21:13:43 -0500

    expand site config

--- Sources/HawkenDotIs/main.swift
+++ Sources/HawkenDotIs/main.swift
@@ -28,4 +28,25 @@ struct HawkenDotIs: Website {
 }
 
 // This will generate your website using the built-in Foundation theme:
-try HawkenDotIs().publish(withTheme: .foundation)
+let path: Path? = nil
+let rssFeedSections: Set<HawkenDotIs.SectionID> = Set(HawkenDotIs.SectionID.allCases)
+let rssFeedConfig: RSSFeedConfiguration? = .default
+let deploymentMethod: DeploymentMethod<HawkenDotIs>? = nil
+let additionalSteps: [PublishingStep<HawkenDotIs>] = []
+let plugins: [Plugin<HawkenDotIs>] = []
+let indentation: Indentation.Kind? = Indentation.Kind.tabs(1)
+
+try HawkenDotIs().publish(using: [
+    .group(plugins.map(PublishingStep.installPlugin)),
+    .optional(.copyResources()),
+    .addMarkdownFiles(),
+    .sortItems(by: \.date, order: .descending),
+    .group(additionalSteps),
+    .generateHTML(withTheme: .foundation, indentation: indentation),
+    .unwrap(rssFeedConfig) { config in
+        .generateRSSFeed(including: rssFeedSections, config: config)
+    },
+    .generateSiteMap(indentedBy: indentation),
+    .unwrap(deploymentMethod, PublishingStep.deploy),
+    .deploy(using: .git("git@github.com:hawkrives/hawken.is.git")),
+])
